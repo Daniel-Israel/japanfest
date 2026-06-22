cebadd961495d20bd9ff1b40505dcc1db7da9013
Daniel Israel <daniel.is.s0ra@gmail.com>
2026-06-22 18:57:43 -0300

    Removendo conexão com fila MQ

--- app/api/api.py
+++ app/api/api.py
@@ -5,18 +5,15 @@ from fastapi import FastAPI
 from app import version
 from app.db.connect import db
 from app.config.log import configure_log
-from app.queue.connect import get_channel
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
 
     configure_log()
-    channel = get_channel()
     db.connect()
 
     yield
-    channel.close()
     db.disconnect()
 
 
