5d8bb89e60288486e98bf6873982848dc8b8d302
Daniel Israel <daniel.is.s0ra@gmail.com>
2026-06-28 17:48:11 -0300

    Adicionando conexão com a impressora no inicio da api

--- app/api/api.py
+++ app/api/api.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from app import version
 from app.db.connect import db
 from app.config.log import configure_log
+from app.print.printer import connect_printer
 
 
 @asynccontextmanager
@@ -12,7 +13,7 @@ async def lifespan(app: FastAPI):
 
     configure_log()
     db.connect()
-
+    connect_printer()
     yield
     db.disconnect()
 
